4d63ec5c52f8321661c43b752b428af6a7627abd
v-Kaefer <vitor.guttler@edu.pucrs.br>
2024-01-23 21:36:19 -0300

    Page cleanup
    - Added 'button' component {shadcn}
    - Removed base page contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Btn from './button.tsx'
+import { Button } from './components/ui/button.tsx'
 
 
 function App() {
@@ -10,24 +8,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='align-middle bg-slate-500'>
+
+        <div><h1>The Radix Hub <img src="src/assets/XRB.png" className=" max-w-8 max-h-8" /></h1></div>
+        <Button>Button</Button>
+        <div className="card">
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-       <Btn />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
