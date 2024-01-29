3b58b3e6db65c0706cf62345041aefbf589df7ea
v-Kaefer <vitor.guttler@edu.pucrs.br>
2024-01-29 18:35:44 -0300

    ui-base-placeholders
    The base for the ui. Really simple, but it's a start!

--- README.md
+++ README.md
@@ -18,3 +18,23 @@ npm create vite@latest
         "scripts": { "test": vitest }
         "scripts": { "test": "vitest --coverage"}
 
+npx tailwindcss init -p
+
+npx shadcn-ui@latest init
+
+√ Would you like to use TypeScript (recommended)? ... no / yes
+√ Which style would you like to use? » New York
+√ Which color would you like to use as base color? » Gray
+√ Where is your global CSS file? ... src/App.css
+√ Would you like to use CSS variables for colors? ... no / yes
+√ Are you using a custom tailwind prefix eg. tw-? (Leave blank if not) ...
+√ Where is your tailwind.config.js located? ... tailwind.config.js
+√ Configure the import alias for components: ... @/components
+√ Configure the import alias for utils: ... @/lib/utils
+√ Are you using React Server Components? ... no / yes
+√ Write configuration to components.json. Proceed? ... yes
+
+* You need to import the component you want, then you can use it...
+- Added 'button' component
+- Added 'menubar' component
+- Added 'skeleton' component
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button.tsx'
+import MenubarDemo from './components/end_components/menubar.tsx'
 
 
 function App() {
-  
 
   return (
-    <>
-      <div className='align-middle bg-slate-500'>
+    <div className='flex-col h-screen w-screen'>
+      <section className='flex flex-col  justify-between bg-slate-800 '>
+        <header className='flex flex-col w-screen min-h-10 max-h-20 shadow-lg shadow-slate-700'>
+          <div className='flex items-center justify-between'>
+            <div className='flex text-center items-center'>
+              <img src="src/assets/XRB.png" className=" m-3 rounded-3xl border-4 max-w-8 max-h-8" />
+              <div className='text-white text-lg font-semibold border-4 border-transparent'>The Radix Hub</div>
+            </div>
+          
+            {/*Colocar lista de items do header (menubar)*/}
+            {/*<span className="text-xl font-bold m-3">Header</span>*/}
+            
+            <div className='w-max items-end'><Button className='m-3'>Launch dApp</Button></div>
+          </div>
+        </header>
 
-        <div><h1>The Radix Hub <img src="src/assets/XRB.png" className=" max-w-8 max-h-8" /></h1></div>
-        <Button>Button</Button>
-        <div className="card">
+        <div title="Content" className='flex bg-slate-700 w-screen h-auto'>
+          <div className='flex px-20 justify-between items-center w-screen h-screen overflow-clip'>
+
+            <div className='text-transparent'>a</div>
+            <h1>The Radix Hub</h1>
+            <div className=' text-transparent'>a</div>
+
+          </div>
         </div>
-      </div>
-    </>
+
+        <footer className='flex flex-col w-screen text-center items-center bg-slate-900 '>
+          <div className="flex w-screen h-auto justify-between px-12">
+            <div title="Footer SNSs" className='flex flex-row m-3 text-white bg-transparent'>
+              <div className='p-2'>discord</div>
+              <div className='p-2'>X</div>
+              <div className='p-2'>whitepaper</div>
+            </div>
+            <div title="Feedback link" className='flex flex-row items-center text-center text-white'>
+              <div>a</div>
+              <div>Feedback</div>
+            </div>
+          </div>
+
+        </footer>
+      </section>
+    </div>
   )
 }
 
